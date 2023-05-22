f5e310bac8fc4c9468c4485f28ae8115deb9276c
Nadyasal <19390100001@dinamika.ac.id>
2023-05-22 08:05:10 +0700

    form pelayanan banmod done

--- resources/views/pelayananBantuanModal/penyaluran/index.blade.php
+++ resources/views/pelayananBantuanModal/penyaluran/index.blade.php
@@ -1,1 +1,21 @@
-penyaluran
+@extends('layout')
+@section('link-active-monitoring','active')
+@section('content')
+<div class="col-md-12 col-sm-12 col-12">
+    <div class="bg-white shadow p-3 rounded-3 my-3">
+        <form action="#" method="post">
+            @csrf
+            <div class="mb-2">
+                <label for="" class="form-label"><b>NIK KPM</b></label>
+                <input type="text" class="form-control" placeholder="No. Urut / NIK KPM" name="id_kpm">
+                <div class="form-text text-danger">
+                    @error('id_kpm') {{$message}} @enderror
+                </div>
+            </div>
+            <div class="d-flex justify-content-end">
+                <button class="btn" style="background-color: #4CBCA1;color:white">Cari</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
