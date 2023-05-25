54d4f7a2698b6ae0b82cf65f1b27f5f1549acbb4
Nadyasal <19390100001@dinamika.ac.id>
2023-05-25 16:46:15 +0700

    form update & show penyaluran

--- resources/views/pelayananBantuanModal/penyaluran/update.blade.php
+++ resources/views/pelayananBantuanModal/penyaluran/update.blade.php
@@ -1,9 +1,10 @@
 @extends('layout')
-@section('link-active-pelayanan-modal','active')
+@section('link-active-pelayanan-modal', 'active')
 @section('content')
 <div class="app-card-body">
     <div class="row">
         <div class="col-12">
+            <h3 class="app-page-title">Upload Bukti Penyaluran Alat Bantu Disabilitas</h3>
             <div class="d-flex justify-content-end mb-3">
                 <a href="{{route('pelayanan.pengajuan.index')}}" class="btn px-2 py-1"
                     style="background-color: #BC4C4C;color:white;width:100px">Kembali</a>
@@ -18,12 +19,12 @@
             </div>
             @endif
 
-            <form action="#" method="post" enctype="multipart/form-data">
+            <form action="{{route('pelayanan.penyaluran.update')}}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="row app-card shadow-sm bg-white p-3">
                     <div class="app-card-body">
                         <div class="g-3">
-                            <h5 class="mb-4">HASIL PENYALURAN ALAT BANTUAN</h5>
+                            <h5 class="mb-4">DATA PENERIMA</h5>
                             <div class="row mb-3">
                                 <label for="" class="col-sm-4 col-form-label">NIK</label>
                                 <div class="col-sm-8">
@@ -69,7 +70,7 @@
                             <div class="row mb-3">
                                 <label for="" class="col-sm-4 col-form-label">Kelurahan</label>
                                 <div class="col-sm-8">
-                                    {{$...->kelurahan}}
+                                    <{{$...->kelurahan}}
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -102,15 +103,17 @@
                                     {{$...->kebutuhan->nama_kebutuhan}}
                                 </div>
                             </div>
-
                             <div class="row mb-3">
-                                <label for="" class="col-sm-4 col-form-label">Dokumentasi Pengajuan</label>
-                                <div class="col-sm-8">
-                                    <img src="{{asset('storage/dokumentasi_pengajuan/'.$...->dokumentasi)}}" alt="" class="img-fluid img-thumbnail" style="object-fit: cover;width:150px">
-                                </div>
+                                <label for="" class="col-sm-4 col-form-label">Dokumentasi Penyaluran</label>
+                                        <div class="col-sm-8">
+                                            <input type="file" class="form-control" name="dokumentasi" id="dokumentasi" accept="image/*" style="min-height:45px">
+                                        </div>
                             </div>
 
-                        
+                            
+                            <div class="d-flex justify-content-end mt-4">
+                                <button class="btn btn-success px-3 py-1">Simpan</button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -118,7 +121,112 @@
         </div>
     </div>
 </div>
-@push('script')
-
-@endpush
 @endsection
+@push('script')
+    <script>
+        $('#kecamatan').on('change',function(e){
+            var id_kecamatan=$(this).find('option:selected').attr('data-idKecamatan');
+            var url="{{route('pelayanan.pengajuan.findKecamatan','')}}/"
+            $.ajax({
+                url:url+id_kecamatan,
+                success:function(kelurahans){
+                    var opt=`<option value="">Silahkan Pilih</option>`
+                    $.each(kelurahans,(index,kelurahan)=>{
+                        opt+=`<option value="${kelurahan.kelurahan}">${kelurahan.kelurahan}</option>`
+                    })
+                    $('#kelurahan').html(opt)
+                }
+            })
+        })
+    </script>
+
+<script>
+    // Delete Alert Confirmation
+    const btn_delete=document.getElementById('btn-delete');
+    btn_delete.addEventListener('click',(ev)=>{
+        if (!confirm('Apakah Yakin Ingin Membatalkan Transaksi ?')) ev.preventDefault()
+    })
+</script>
+
+<script>
+const MAX_WIDTH = 1200;
+const MAX_HEIGHT = 1200;
+const MIME_TYPE = "image/jpeg";
+const QUALITY = 0.7;
+
+const dokumentasi=document.getElementById('dokumentasi')
+
+dokumentasi.addEventListener('change',(ev)=>{
+    const file = ev.target.files[0]; // get the file
+    const blobURL = URL.createObjectURL(file);
+    const img = new Image();
+
+    img.src=blobURL
+
+    img.onerror=()=>{
+        URL.revokeObjectURL(this.src)
+        alert('Cannot Load Image');
+    }
+
+    img.onload=function(){
+        URL.revokeObjectURL(this.src)
+        const [newWidth, newHeight] = calculateSize(img, MAX_WIDTH, MAX_HEIGHT);
+        const canvas = document.createElement("canvas");
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+        
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, newWidth, newHeight);
+        canvas.toBlob(
+            (blob) => {
+
+                const newImage=new File([blob],file.name)
+                const dataTransfer=new DataTransfer()
+                dataTransfer.items.add(newImage)
+                //set image to input file data_compressed
+                document.getElementById('dokumentasi').files=dataTransfer.files
+            },
+            MIME_TYPE,
+            QUALITY
+        );
+
+        // document.getElementById("root").append(canvas);
+    }
+})
+
+
+function calculateSize(img, maxWidth, maxHeight) {
+    let width = img.width;
+    let height = img.height;
+
+    // calculate the width and height, constraining the proportions
+    if (width > height) {
+        if (width > maxWidth) {
+            height = Math.round((height * maxWidth) / width);
+            width = maxWidth;
+        }
+    } else {
+        if (height > maxHeight) {
+            width = Math.round((width * maxHeight) / height);
+            height = maxHeight;
+        }
+    }
+    return [width, height];
+}
+
+// Utility functions for demo purpose
+
+function displayInfo(label, file) {
+    const p = document.createElement('p');
+    p.innerText = `${label} - ${readableBytes(file.size)}`;
+    document.getElementById('root').append(p);
+}
+
+function readableBytes(bytes) {
+    const i = Math.floor(Math.log(bytes) / Math.log(1024)),
+    sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
+}
+</script>
+@endpush 
