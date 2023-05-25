0cd7d6cd01b2734cad98ffa607eba332e5c1820f
reztsky <sultaninot@gmail.com>
2023-05-25 16:52:43 +0700

    Pelayanan Modul:penyaluran

---
+++ app/Models/penyaluranKebutuhan.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class penyaluranKebutuhan extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $fillable=[
+        'id_pengajuan',
+        'bap',
+        'foto_penyaluran',
+    ];
+
+    public function pengajuan(){
+        return $this->belongsTo(PengajuanKebutuhan::class,'id_pengajuan','id');
+    }
+}
