f54e743bbcf4273af12bbc57af9b0f1b46e435a9
reztsky <sultaninot@gmail.com>
2023-08-21 11:54:18 +0700

    Fix Captcha

--- config/captchax.php
+++ config/captchax.php
@@ -1,14 +1,13 @@
 <?php
 
 return [
-    'disable' => env('CAPTCHA_DISABLE', false),
+    'disable' =>env('CAPTCHA_DISABLE', !str_contains(env('APP_ENV', 'local'), 'prod')),
     'characters' => ['2', '3', '4', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
     'default' => [
         'length' => 9,
         'width' => 120,
         'height' => 36,
         'quality' => 90,
-        'math' => false,
         'expire' => 60,
         'encrypt' => false,
     ],
