3832f6cfac97f4813368da2c3d74942a0a3acf08
reztsky <sultaninot@gmail.com>
2023-11-02 14:22:58 +0700

    fix minor bug

--- resources/views/components/bantuan-modal/dashboard/detail-are-null.blade.php
+++ resources/views/components/bantuan-modal/dashboard/detail-are-null.blade.php
@@ -1,7 +1,6 @@
 <td>
     @if (!is_null($fileName))
-        <a href="{{ asset('storage/' . $folder . '/' . $fileName) }}" target="_blank" rel="noopener noreferrer">Foto
-            KPM</a><br>
+        <a href="{{ asset('storage/' . $folder . '/' . $fileName) }}" target="_blank" rel="noopener noreferrer">{{str_replace('_', ' ', Str::upper($folder))}}</a><br>
     @else
         <a target="_blank" rel="noopener noreferrer">-</a><br>
     @endif
