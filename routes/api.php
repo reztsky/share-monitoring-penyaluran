5f7397cd0d2aa45c7b7a3e67d82099227dbee525
reztsky <sultaninot@gmail.com>
2023-12-05 10:40:04 +0700

    share-it

--- routes/api.php
+++ routes/api.php
@@ -19,23 +19,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::group([
-    'as'=>'apiBantuanModal.',
-    'prefix'=>'bantuan-modal/',
-    'controller'=>ApiBantuanModalController::class,
-], function(){
-    Route::get('/','index')->name('index');
-});
-
-Route::group([
-    'as'=>'apiAlatBantu.',
-    'prefix'=>'alat-bantu/',
-    'controller'=>ApiAlatBantuController::class
-], function(){
-    Route::post('/','index')->name('index');
-    Route::post('/all','all')->name('all');
-    Route::post('detail/penyaluran/{jenis_bantuan}/','detailPenyaluran')->name('detailPenyaluran');
-    Route::post('detail/pengajuan/{jenis_bantuan}/','detailPengajuan')->name('detailPengajuan');
-    // Route::post('/count-gender','countByGender')->name('countByGender');
-});
